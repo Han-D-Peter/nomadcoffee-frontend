65cfcb888d910d693942f2d1ee24568a302a4f9b
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-12-15 01:44:46 +0900

    sign up & log in

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { useReactiveVar } from "@apollo/client";
-import { darkModeVar, isLoggedInVar } from "./apollo";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import { client, darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, GlobalStyles, lightTheme } from "./styles";
+import { HelmetProvider } from "react-helmet-async";
+import routes from "./routes";
+import SignUp from "./screens/SignUp";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <GlobalStyles />
-      <Router>
-        <Route path="/" exact>
-          {isLoggedIn ? <Home /> : <Login />}
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Router>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router>
+            <Switch>
+              <Route path={routes.home} exact>
+                {isLoggedIn ? <Home /> : <Login />}
+              </Route>
+              {!isLoggedIn ? (
+                <Route path={routes.signUp}>
+                  <SignUp />
+                </Route>
+              ) : null}
+              <Route>
+                <NotFound />
+              </Route>
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,10 @@
-import { isLoggedInVar } from "../apollo";
+import { logUserOut } from "../apollo";
 
-const Home = ({ setIsLoggedIn }) => {
+const Home = () => {
   return (
     <div>
       <h1>Home!</h1>
-      <button onClick={()=isLoggedInVar(false)}>Log out now!</button>
+      <button onClick={() => logUserOut(false)}>Log out now!</button>
     </div>
   );
 };
