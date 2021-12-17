e1bebafbadb0053d8cea3e8f5795bf17bf6de47f
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-12-17 21:57:47 +0900

    add uri for production

--- src/apollo.js
+++ src/apollo.js
@@ -34,7 +34,12 @@ export const disableDarkMode = () => {
   darkModeVar(false);
 };
 
-const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
+const httpLink = new HttpLink({
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://instaclone-backend-peter.herokuapp.com/graphql"
+      : "http://localhost:4000/graphql",
+});
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   // add the authorization to the headers
